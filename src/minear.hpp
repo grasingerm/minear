08d89e8e3c1c60bfc1b3848a874e88381363a053
Matthew Grasinger <grasingerm@gmail.com>
2014-07-29 03:43:05 -0400

    fixed problem with type

--- src/minear.hpp
+++ src/minear.hpp
@@ -82,7 +82,7 @@ namespace minear
         
         for (unsigned int i = 0; i < n_rows; i++)
             for (unsigned int j = 0; j < n_cols; j++)
-                data[i*n_rows + j] = a(i,j);
+                p[i*n_rows + j] = a(i,j);
         
         delete[] data;
         data = p;
